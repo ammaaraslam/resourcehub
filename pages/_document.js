b94d2ba8ca448a44c3eabaf0c9525e9e0fce499c
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-07-31 07:50:44 +0530

    added external fonts to _document.js

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.cdnfonts.com/css/clash-display"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.cdnfonts.com/css/sf-ui-display"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
