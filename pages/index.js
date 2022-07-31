a56ae14769e8991342f8ad3f4a0409a6e6d6b955
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-07-31 20:17:14 +0530

    styled homepage

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ export default function Home() {
       <main className="w-full h-full bg-white dark:bg-black">
         <div className="pt-56 flex">
           <div className="ml-auto mr-auto md:max-w-6xl max-w-4xl text-center">
-            <h1 className="p-2 text-black dark:text-white font-extrabold md:text-8xl text-5xl tracking-wider font-lalezar uppercase">
+            <h1 className="p-2 text-black dark:text-white font-extrabold md:text-7xl text-5xl tracking-wider font-lalezar uppercase">
               A place for all{" "}
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-indigo-300 to-purple-600">
                 developer resources
@@ -86,15 +86,15 @@ export default function Home() {
         <div className="md:py-14 py-11 md:mt-56 mt-40 px-3  w-11/12 ml-auto mr-auto rounded-3xl bg-black dark:bg-white bg-opacity-10 dark:bg-opacity-10">
           <div className=" w-full h-fit p-4">
             <div className="w-3/4 ml-auto mr-auto">
-              <h1 className="p-2 text-center text-black dark:text-white font-black md:text-5xl text-3xl tracking-wider font-lalezar uppercase">
+              <h1 className="p-2 text-center text-black dark:text-white font-black md:text-5xl text-3xl font-lalezar uppercase">
                 <span className="font-extrabold opacity-80 dark:opacity-80">
-                  Find the right resources{" "}
+                  Find the right resources
                 </span>
                 <br></br>for yourself
               </h1>
             </div>
             <div className="md:p-8 p-5 bg-black dark:bg-white text-white dark:text-black rounded-xl md:flex block justify-between w-11/12 ml-auto mr-auto mt-5">
-              <p className="font-montserrat font-bold md:text-4xl text-3xl md:max-w-xs md:text-right text-left mt-auto mb-auto">
+              <p className="font-montserrat font-bold md:text-4xl text-3xl md:max-w-xs text-left mt-auto mb-auto">
                 Every type of resource you can imagine
               </p>
 
@@ -108,19 +108,19 @@ export default function Home() {
             </div>
           </div>
           <div className="mt-24 w-full h-fit p-4">
-            <div className="w-8/12 ml-auto mr-auto">
-              <h1 className="p-2 text-center text-black dark:text-white font-black md:text-5xl text-3xl tracking-wider font-lalezar uppercase">
+            <div className="w-3/4 ml-auto mr-auto">
+              <h1 className="p-2 text-center text-black dark:text-white font-black md:text-5xl text-3xl font-lalezar uppercase">
                 <span className="font-extrabold opacity-80 dark:opacity-80">
-                  Share resources you find{" "}
+                  Share resources you find
                 </span>
                 <br></br>interesting
               </h1>
             </div>
             <div className="md:p-8 p-5 bg-black dark:bg-white text-white dark:text-black rounded-xl md:flex block justify-between md:w-4/5 w-11/12 ml-auto mr-auto mt-5">
               <p className="font-montserrat font-bold md:text-4xl text-3xl md:max-w-xs text-left mt-auto mb-auto">
-                Find a resource on the web? Share it with others
+                Found a useful resource on the web? Share it with others
               </p>
-              <div className="font-montserrat font-medium text-xl md:w-2/5 w-full md:mt-0 mt-2">
+              <div className="font-montserrat font-medium md:text-2xl text-xl md:w-1/2 w-full mt-auto mb-auto">
                 <p>
                   Easily and quickly fill a simple form with just 4 fields and
                   you have successfully added your resource to our database.
@@ -129,10 +129,10 @@ export default function Home() {
             </div>
           </div>
           <div className="mt-24 w-full h-fit p-4">
-            <div className="w-8/12 ml-auto mr-auto">
-              <h1 className="p-2 text-center text-black dark:text-white font-black md:text-5xl text-3xl tracking-wider font-lalezar uppercase">
+            <div className="w-3/4 ml-auto mr-auto">
+              <h1 className="p-2 text-center text-black dark:text-white font-black md:text-5xl text-3xl font-lalezar uppercase">
                 <span className="font-extrabold opacity-80 dark:opacity-80">
-                  What are you waiting for?{" "}
+                  What are you waiting for?
                 </span>
                 <br></br>Get Started
               </h1>
@@ -142,7 +142,7 @@ export default function Home() {
                 Quickly start using this app in 2 simple steps
               </p>
 
-              <div className="font-montserrat font-medium text-xl md:w-2/5 w-full md:mt-0 mt-2">
+              <div className="font-montserrat font-medium md:text-2xl text-xl md:w-1/2 w-full md:mt-0 mt-2">
                 <ul>
                   <li>1️⃣ Sign Up with a GitHub/Google account.</li>
                   <li className="mt-2">
@@ -163,17 +163,19 @@ export default function Home() {
           </div>
         </div>
         <div className="md:mt-24 mt-20 w-full h-fit p-4">
-          <h1 className="p-2 text-black dark:text-white font-black md:text-5xl text-4xl tracking-wider font-lalezar uppercase">
+          <h1 className="pt-2 px-2 text-black dark:text-white font-black md:text-5xl text-4xl font-lalezar uppercase">
             Latest Resources
           </h1>
-          {resourcesLoading ? (
-            <SkeletonFeed
-              resourceLoading={resourcesLoading}
-              skeletonCards={skeletonCards}
-            />
-          ) : (
-            <Feed resources={resources} />
-          )}
+          <div className="-mt-10">
+            {resourcesLoading ? (
+              <SkeletonFeed
+                resourceLoading={resourcesLoading}
+                skeletonCards={skeletonCards}
+              />
+            ) : (
+              <Feed resources={resources} />
+            )}
+          </div>
           <div className="flex justify-center items-center ml-auto mr-auto pb-6">
             <OutlinedButton
               handleOnClick={() => {
